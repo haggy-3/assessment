532690d4e932bc7ad1ad740f64add892d97df167
haggy-3 <63593259+haggy-3@users.noreply.github.com>
2020-04-13 22:40:40 +0900

    #1に対する対応を行いました

--- assessment.js
+++ assessment.js
@@ -19,7 +19,7 @@ assessmentButton.onclick = () => {
   if (userName.length === 0) { // 名前が空の時は処理を終了する
     return;
   }
-
+ 
   // 診断結果表示エリアの作成
   removeAllChildren(resultDivided);
   const header = document.createElement('h3');
